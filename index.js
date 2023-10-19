2d7307da3ef109cfddc7aa063207d3bc085fda69
Gaute <mrboobai@proton.me>
2023-10-19 21:12:27 +0200

    added weather speed

--- index.js
+++ index.js
@@ -39,83 +39,97 @@ client.on("interactionCreate", async (interaction) => {
 
   if (interaction.commandName === "weather") {
     const axios = require("axios");
-  
-    async function getWeather() {
-      const response = await axios.get(
-        "https://api.open-meteo.com/v1/forecast?latitude=59.92&longitude=5.45&hourly=temperature_2m,precipitation_probability,precipitation&current_weather=true&forecast_days=1&timezone=auto"
-      );
-      const json = response.data;
-      var temperature = json.current_weather.temperature;
-      temperature += 4.5;
-      return temperature.toString();
-    }
-  
-    weather = await getWeather();
-    return interaction.reply(weather + " " + "°C");
-  }
-  
-  if (interaction.commandName === "coinflip") {
-    function getCoin() {
-      return Math.floor(Math.random() * 2) + 1;
+
+    async function fetchWeather() {
+      try {
+        const response = await axios.get(
+          "https://api.open-meteo.com/v1/forecast?latitude=59.92&longitude=5.45&hourly=temperature_2m,precipitation_probability,precipitation&current_weather=true&forecast_days=1&timezone=auto"
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+        return null;
+      }
     }
 
-    var coin = getCoin();
+    async function returnWeather() {
+      const weatherData = await fetchWeather();
+      if (weatherData) {
+        const temperature = weatherData.current_weather.temperature + 4.5;
+        const windspeed = weatherData.current_weather.windspeed;
+        const responseText = `${temperature} °C, ${windspeed} km/h`;
+        interaction.reply(responseText);
+      } else {
+        // Handle the case where the API request fails
+        interaction.reply("Failed to fetch weather data.");
+      }
+    }
 
-    if (coin == 1) {
-      return interaction.reply("Head Wins!");
-    }
-    if (coin == 2) {
-      return interaction.reply("Tails Wins!");
-    }
+    returnWeather();
   }
-  if (interaction.commandName === "randomnumber100") {
-    function getRandomNumber100() {
-      var randomNumber100 = Math.floor(Math.random() * 100) + 1;
-      return randomNumber100.toString();
+
+    if (interaction.commandName === "coinflip") {
+      function getCoin() {
+        return Math.floor(Math.random() * 2) + 1;
+      }
+
+      var coin = getCoin();
+
+      if (coin == 1) {
+        return interaction.reply("Head Wins!");
+      }
+      if (coin == 2) {
+        return interaction.reply("Tails Wins!");
+      }
     }
-    var randomNumberString100 = getRandomNumber100();
-    return interaction.reply(randomNumberString100);
-  }
-  if (interaction.commandName === "randomnumber50") {
-    function getRandomNumber50() {
-      var randomNumber50 = Math.floor(Math.random() * 50) + 1;
-      return randomNumber50.toString();
+    if (interaction.commandName === "randomnumber100") {
+      function getRandomNumber100() {
+        var randomNumber100 = Math.floor(Math.random() * 100) + 1;
+        return randomNumber100.toString();
+      }
+      var randomNumberString100 = getRandomNumber100();
+      return interaction.reply(randomNumberString100);
     }
-    var randomNumberString50 = getRandomNumber50();
-    return interaction.reply(randomNumberString50);
-  }
-  if (interaction.commandName === "randomnumber25") {
-    function getRandomNumber25() {
-      var randomNumber25 = Math.floor(Math.random() * 25) + 1;
-      return randomNumber25.toString();
+    if (interaction.commandName === "randomnumber50") {
+      function getRandomNumber50() {
+        var randomNumber50 = Math.floor(Math.random() * 50) + 1;
+        return randomNumber50.toString();
+      }
+      var randomNumberString50 = getRandomNumber50();
+      return interaction.reply(randomNumberString50);
     }
-    var randomNumberString25 = getRandomNumber25();
-    return interaction.reply(randomNumberString25);
-  }
-  if (interaction.commandName === "randomnumber10") {
-    function getRandomNumber10() {
-      var randomNumber10 = Math.floor(Math.random() * 10) + 1;
-      return randomNumber10.toString();
+    if (interaction.commandName === "randomnumber25") {
+      function getRandomNumber25() {
+        var randomNumber25 = Math.floor(Math.random() * 25) + 1;
+        return randomNumber25.toString();
+      }
+      var randomNumberString25 = getRandomNumber25();
+      return interaction.reply(randomNumberString25);
     }
-    var randomNumberString10 = getRandomNumber10();
-    return interaction.reply(randomNumberString10);
-  }
-  if (interaction.commandName === "randomnumber5") {
-    function getRandomNumber5() {
-      var randomNumber5 = Math.floor(Math.random() * 5) + 1;
-      return randomNumber5.toString();
+    if (interaction.commandName === "randomnumber10") {
+      function getRandomNumber10() {
+        var randomNumber10 = Math.floor(Math.random() * 10) + 1;
+        return randomNumber10.toString();
+      }
+      var randomNumberString10 = getRandomNumber10();
+      return interaction.reply(randomNumberString10);
     }
-    var randomNumberString5 = getRandomNumber5();
-    return interaction.reply(randomNumberString5);
-  }
-  if (interaction.commandName === "randomnumber1000") {
-    function getRandomNumber1000() {
-      var randomNumber1000 = Math.floor(Math.random() * 1000) + 1;
-      return randomNumber1000.toString();
+    if (interaction.commandName === "randomnumber5") {
+      function getRandomNumber5() {
+        var randomNumber5 = Math.floor(Math.random() * 5) + 1;
+        return randomNumber5.toString();
+      }
+      var randomNumberString5 = getRandomNumber5();
+      return interaction.reply(randomNumberString5);
     }
-    var randomNumberString1000 = getRandomNumber1000();
-    return interaction.reply(randomNumberString1000);
-  }
-});
+    if (interaction.commandName === "randomnumber1000") {
+      function getRandomNumber1000() {
+        var randomNumber1000 = Math.floor(Math.random() * 1000) + 1;
+        return randomNumber1000.toString();
+      }
+      var randomNumberString1000 = getRandomNumber1000();
+      return interaction.reply(randomNumberString1000);
+    }
+  });
 
 client.login(process.env.token);
