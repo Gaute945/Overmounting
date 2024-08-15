34881a3584b92fe54b01ddefc8f078d6709d984c
Gaute <mrboobai@proton.me>
2024-08-15 17:36:25 +0200

    added esLint and fixed package-lock

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // index.js
-const fs = require('node:fs');
-const path = require('node:path');
-const { Events } = require('discord.js');
-const client = require('./client');
+const fs = require("node:fs");
+const path = require("node:path");
+const { Events } = require("discord.js");
+const client = require("./client");
 
-const foldersPath = path.join(__dirname, 'commands');
+// __dirname is defined
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  const commandFiles = fs.readdirSync(commandsPath).filter(file =>
+    file.endsWith(".js"));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
+    if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -40,20 +42,21 @@ client.on(Events.InteractionCreate, async interaction => {
   } catch (error) {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+      await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
     } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
     }
   }
 });
 
 client.on("ready", async () => {
-  const botTestingChannel = client.channels.cache.find(channel => channel.name === 'bot-testing');
+  const botTestingChannel = client.channels.cache.find(channel =>
+    channel.name === "bot-testing");
   if (botTestingChannel) {
-    botTestingChannel.send('I am now in use!');
-    console.log('Message sent to bot-testing channel.');
+    botTestingChannel.send("I am now in use!");
+    console.log("Message sent to bot-testing channel.");
   } else {
-    console.error('Bot-testing channel not found!');
+    console.error("Bot-testing channel not found!");
   }
 });
 
